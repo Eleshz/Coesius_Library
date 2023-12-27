5365706b29f71ee5857b812fdb3b4a17e8012e99
Edgar Hammond <edgarhammond@gmail.com>
2023-12-27 16:36:51 -0500

    Reverting to change code architecture

--- Sia/Sia_layer.cpp
+++ Sia/Sia_layer.cpp
@@ -1,5 +1,4 @@
 #pragma once
 
 #include "Sia_network.hpp"
-
 #include <Eigen/Dense>
--- Sia/Sia_network.hpp
+++ Sia/Sia_network.hpp
@@ -6,6 +6,7 @@ class Sia_layered_network
 private:
     
 public:
+
     Sia_layered_network();
     ~Sia_layered_network();
 };
