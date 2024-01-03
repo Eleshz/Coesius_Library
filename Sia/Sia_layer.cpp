e3c21a5f541c70992b23462d2b806c0bfc16c2a9
Edgar Hammond <edgarhammond@gmail.com>
2024-01-03 17:16:51 -0500

    Undo Undo Undo

--- Sia/Sia_layer.cpp
+++ Sia/Sia_layer.cpp
@@ -1,4 +1,2 @@
-#pragma once
-
 #include "Sia_network.hpp"
 #include <Eigen/Dense>
--- Sia/Sia_network.hpp
+++ Sia/Sia_network.hpp
@@ -1,12 +1,23 @@
 #include <Eigen/Dense>
+#include <variant>
 
 #pragma once
+
+class Layer
+{
+public:
+    Layer();
+    ~Layer();
+};
+
 class Sia_layered_network
 {
 private:
-    
-public:
 
+public:
     Sia_layered_network();
     ~Sia_layered_network();
+
+    
 };
+
