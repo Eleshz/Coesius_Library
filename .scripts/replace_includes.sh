c7a95664fd69e2d52b77692c781b4f7cc7c36e28
Edgar Hammond <edgarhammond@gmail.com>
2024-02-04 22:05:07 -0500

    Fixed the "replace_includes.sh" script for the new file structure lol. THANKS GPT4 :)

--- .scripts/replace_includes.sh
+++ .scripts/replace_includes.sh
@@ -40,15 +40,20 @@ if [ ! -f $dest_dir/Sia_network_full.hpp ]; then
 fi
 
 # For each .ipp include found
-grep -o '#include ".*\.ipp"' $dest_dir/network_full.hpp | while read -r line ; do
+grep -o '#include <.*\.ipp>' $dest_dir/network_full.hpp | while read -r line ; do
     # Extract the .ipp filename
-    filename=$(echo $line | cut -d'"' -f 2)
+    filename=$(echo $line | cut -d'<' -f 2 | cut -d'>' -f 1)
+
+    # Remove the first directory from the filename
+    filename=${filename#*/}
+
+    # Escape special characters in the line for use in awk
+    line_escaped=$(echo $line | sed 's/[]\/$*.^[]/\\&/g')
 
     # Check if the .ipp file exists in the source directory
     if [ -f $src_dir/$filename ]; then
         # Replace the include line with the content of the .ipp file
-        sed -i "/$line/r $src_dir/$filename" $dest_dir/network_full.hpp
-        sed -i "/$line/d" $dest_dir/network_full.hpp
+        awk -v r="$src_dir/$filename" "/$line_escaped/{while((getline < r) > 0) print; next}1" $dest_dir/network_full.hpp > temp && mv temp $dest_dir/network_full.hpp
     else
         echo "File $filename not found in $src_dir!"
     fi
--- AUTO_RELEASES/Sia_Lib_AUTO-0.00.04.hpp
+++ AUTO_RELEASES/Sia_Lib_AUTO-0.00.04.hpp
@@ -10,7 +10,21 @@
 #include <iostream>
 #include <algorithm>
 
-#include <sia/general.ipp>
+
+#pragma once
+
+#include <random>
+
+static struct RandomUInt64T {
+    std::mt19937_64 _gen;
+    std::uniform_int_distribution<uint64_t> _dis;
+
+    RandomUInt64T() : _gen(std::random_device{}()), _dis(std::numeric_limits<uint64_t>::min(), std::numeric_limits<uint64_t>::max()) {}
+
+    uint64_t operator()() {
+        return _dis(_gen);
+    }
+} RANDOM_UINT64T;
 
 enum layer_types {
     INPUT = 0,
@@ -99,7 +113,40 @@ public:
     
 };
 
-#include <sia/input.ipp>
+template <Sia::INTERNAL::is_valid S>
+template <typename U>
+constexpr uint16_t Sia::Input_matrix<S>::deduce_input_type() {
+    if constexpr (Sia::INTERNAL::is_valid_1D_matrix<U>) {
+        return 1;
+    } else if constexpr (Sia::INTERNAL::is_valid_2D_matrix<U>) {
+        return 2;
+    } else if constexpr (Sia::INTERNAL::is_valid_3D_matrix<U>) {
+        return 3;
+    }
+    return 4; 
+}
+
+template <Sia::INTERNAL::is_valid S>
+void Sia::Input_matrix<S>::debugStatus(){
+    bool sync_status = std::ios::sync_with_stdio();
+    std::ios::sync_with_stdio(false);
+    std::cout << "The ID of this layer is: " << _ID << "\n";
+    switch (_input_type) {
+    case (1):
+        std::cout << "One dimensional Eigen array/matrix used\n";
+        break;
+    case (2):
+        std::cout << "Two dimensional Eigen array/matrix used:\n";
+        break;
+    case (3):
+        std::cout << "Three dimensional Eigen array/matrix used\n";
+
+        break;
+    default:
+        break;
+    }
+    std::ios::sync_with_stdio(sync_status);
+}
 
 class Dense_layer {
     friend Layered_network;
@@ -192,6 +239,124 @@ public:
     
 };
 
-#include <sia/network.ipp>
+#include <algorithm>
+
+void Sia::Layered_network::delete_layer(const uint64_t arg_ID) {
+    if (_layers.empty() || !existing_ID(arg_ID)) {
+        return;
+    }
+    std::ptrdiff_t index = 0;
+    for (auto &layer : _layers) {
+        if (std::get<1>(layer) == arg_ID) {
+            u_int16_t layer_type = std::get<0>(layer);
+            u_int32_t setting_index = std::get<2>(layer);
+            _layers.erase(_layers.begin() + index);
+            if(layer_type == 1) {
+                settings_delete(layer_types::INPUT, setting_index);
+            }
+            std::for_each(_layers.begin() + index, _layers.end(), [&](auto& lambda_layer){--std::get<2>(lambda_layer);});
+            break;
+        }
+        ++index;
+    }
+}
+
+void Sia::Layered_network::settings_delete(const uint8_t arg_type, const uint32_t arg_index) {
+    switch (arg_type)
+    {
+    case layer_types::INPUT:
+        std::get<0>(_input_settings) = 0;
+        std::get<1>(_input_settings) = matrix_types::type_NAN;
+        _dense_settings.erase(_dense_settings.begin() + arg_index);
+        break;
+    case layer_types::DENSE:
+
+    default:
+        break;
+    }
+}
+
+bool Sia::Layered_network::existing_ID(const u_int64_t& arg_ID) {
+    for (auto layer : _layers){
+        if(std::get<1>(layer) == arg_ID)
+            return true;
+    }
+    return false;
+}
+
+void Sia::Layered_network::conditional_reserve(){
+
+    // Both 3 and 10 are 'randomly' chosen numbers, 3 seems like a
+    // fair ~buffer~ before reallocating more memory, and 10 seems
+    // like a good chunk to reserve
+
+    if ((_layers.capacity()-_current_working_index_general)<=3) {
+        _layers.reserve(_layers.capacity()+10);
+        _dense_settings.reserve(_dense_settings.capacity()+10);
+    }
+    if ((_arrays.capacity()-_current_working_index_array)<=3) {
+        _arrays.reserve(_arrays.capacity()+10);
+    }
+    if ((_matrices.capacity()-_current_working_index_matrix)<=3) {
+        _matrices.reserve(_matrices.capacity()+10);
+    }
+}
+
+template <Sia::INTERNAL::is_valid_1D_matrix T>
+void Sia::Layered_network::add_layer(const Sia::Input_matrix<T>& arg_layer) {
+
+    conditional_reserve();
+
+    for (auto layer_i : _layers){
+        if(existing_ID(arg_layer._ID)) {
+            std::cerr << "Only one input layer permitted... this has done nothing!\n";
+            return;
+        }
+    }
+    _layers.emplace_back(INPUT, (arg_layer._ID), _current_working_index_general);
+    _arrays[_current_working_index_array] = arg_layer._input;
+    ++_current_working_index_array;
+    ++_current_working_index_general;
+}
+
+template <Sia::INTERNAL::is_valid_2D_matrix T>
+void Sia::Layered_network::add_layer(const Sia::Input_matrix<T>& arg_layer) {
+    
+    conditional_reserve();
+
+    for (auto layer_i : _layers){
+        if(existing_ID(arg_layer._ID)) {
+            std::cerr << "Only one input layer permitted... this has done nothing!\n";
+            return;
+        }
+    }
+    _layers.emplace_back(INPUT, (arg_layer._ID), _current_working_index_general);
+    _matrices[_current_working_index_matrix] = arg_layer._input;
+    ++_current_working_index_matrix;
+    ++_current_working_index_general;
+}
+
+template <Sia::INTERNAL::is_valid_3D_matrix T>
+void Sia::Layered_network::add_layer(const Sia::Input_matrix<T>& arg_layer) {
+    
+    conditional_reserve();
+
+    const std::size_t first_layer = 0;
+    const std::size_t second_layer = 1;
+    const std::size_t third_layer = 2;
+
+    for (std::tuple<layer_types, uint64_t, uint64_t> layer_i : _layers){
+        if(existing_ID(std::get<2>(layer_i))) {
+            std::cerr << "Only one input layer permitted... this has done nothing!\n";
+            return;
+        }
+    }
+    _layers.emplace_back(INPUT, (arg_layer._ID), _current_working_index_general);
+    _matrices[_current_working_index_matrix] = arg_layer._input[first_layer];
+    _matrices[_current_working_index_matrix+1] = arg_layer._input[second_layer];
+    _matrices[_current_working_index_matrix+2] = arg_layer._input[third_layer];
+    _current_working_index_matrix += 3;
+    ++_current_working_index_general;
+}
 
 } // Namespace end scope
